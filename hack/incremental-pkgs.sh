ea006237d6268d063a6c74b10733c167715cad1d
yunshihang <shihang.yun@daocloud.io>
2022-11-16 11:17:26 +0800

    Update incremental-pkgs.sh

--- hack/incremental-pkgs.sh
+++ hack/incremental-pkgs.sh
@@ -92,6 +92,7 @@ mv manifest.yml manifest-${DATE}.yml
 tar zcvf kubean-incremental-${DATE}.tar.gz $(pwd)/data
 sha512sum kubean-incremental-${DATE}.tar.gz > kubean-incremental-${DATE}-checksum.txt
 
+ssh-keygen -R ${OFFLINE_NGINX_IP}
 sshpass -p ${OFFLINE_NGINX_PASSWORD} scp -rp kubean-incremental-${DATE}-${FILE_SUFFIX}-checksum.txt root@${OFFLINE_NGINX_IP}:/root/release-2.19-offline/offline-files/kubean-incremental-package/ && \
 echo "Success Upload checksum file to intranet nginx"
 sshpass -p ${OFFLINE_NGINX_PASSWORD} scp -rp kubean-incremental-${DATE}-${FILE_SUFFIX}.tar.gz root@${OFFLINE_NGINX_IP}:/root/release-2.19-offline/offline-files/kubean-incremental-package/ && \
